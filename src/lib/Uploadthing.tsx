9410bb882ac140550cd963d582283e22afd9aa5d
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-12-13 00:25:59 +0530

    uploadthing issues

--- src/lib/Uploadthing.tsx
+++ src/lib/Uploadthing.tsx
@@ -1,6 +1,5 @@
 import { generateUploadButton } from "@uploadthing/react";
 import { OurFileRouter } from "@/api/src/uploadthing";
 
-export const UploadButton = generateUploadButton<OurFileRouter>({
-  url: `http://localhost:3000/api/uploadthing`, // Remove the v1 path segment
+export const UploadButton = generateUploadButton<OurFileRouter>({ // Remove the v1 path segment
 });
