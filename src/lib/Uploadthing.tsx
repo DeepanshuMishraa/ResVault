0c092cae0580f070f2574e1f9fecc46655f8f933
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-12-15 12:44:57 +0530

    rm unnec

--- src/lib/Uploadthing.tsx
+++
@@ -1,6 +0,0 @@
-import { generateUploadButton } from "@uploadthing/react";
-import { OurFileRouter } from "@/api/src/uploadthing";
-
-export const UploadButton = generateUploadButton<OurFileRouter>({
-    
-});
