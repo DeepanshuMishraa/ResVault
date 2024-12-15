116bcab311dcc91c1c1c5da58f887c2540009662
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-12-15 13:11:22 +0530

    vercel configs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./lib/AuthContext";
 import { Toaster } from "./components/ui/toaster";
 import AppRoutes from "./routes";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -10,9 +11,10 @@ function App() {
         <div className="min-h-screen">
           <AppRoutes />
           <Toaster />
+          <Analytics />
         </div>
       </AuthProvider>
-    </Router>       
+    </Router>
   );
 }
 
